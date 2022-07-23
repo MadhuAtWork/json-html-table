a8589a03feff830e96d76f75f5c9b1e7bd6f8830
MadhuAtWork <109514968+MadhuAtWork@users.noreply.github.com>
2022-07-23 22:19:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TableData from "./Component/TableData";
+
+function App() {
+  return (
+    <div className="App">
+<TableData></TableData>
+    </div>
+  );
+}
+
+export default App;
